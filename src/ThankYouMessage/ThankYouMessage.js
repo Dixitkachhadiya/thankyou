09ee677a2631b096b08e8960348883e425652513
Dixit Kachhadiya <dixitkachhadiya21@gmail.com>
2024-09-01 12:05:52 +0530

    third-commit

--- src/ThankYouMessage/ThankYouMessage.js
+++ src/ThankYouMessage/ThankYouMessage.js
@@ -1,10 +1,17 @@
-import React from "react";
+import React,{useEffect} from "react";
 import "./ThankYouMessage.css";
-
+import AOS from "aos";
+import "aos/dist/aos.css";
 const ThankYouMessage = () => {
+
+  useEffect(() => {
+    AOS.init({
+      duration: 1000, // Duration of animation in milliseconds
+    });
+  }, []);
   return (
     <div className="thank-you-container">
-      <div className="thank-you-message">
+      <div className="thank-you-message" data-aos="zoom-in">
         <h1>Thank You for the Birthday Wishes ❤️😀</h1>
         <p>Your thoughtfulness made my day extra special.😊</p>
         <p>By Dixit Kachhadiya 🎂</p>
